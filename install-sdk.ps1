14180feaf2b2754d27e4bc753c4cf3964d77f58d
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-24 16:38:11 -0400

    Add files via upload

---
+++ install-sdk.ps1
@@ -0,0 +1,18 @@
+param(
+    [string]$SdkVersion
+)
+
+# Define the URL for the Windows SDK installer
+$SdkUrl = "https://go.microsoft.com/fwlink/?linkid=2261842"
+
+# Define the path where the SDK installer will be saved
+$InstallerPath = "$env:TEMP\windows-sdk-setup.exe"
+
+# Download the Windows SDK installer
+Invoke-WebRequest -Uri $SdkUrl -OutFile $InstallerPath
+
+# Execute the installer
+Start-Process -FilePath $InstallerPath -Wait
+
+# Clean up: delete the installer file
+Remove-Item -Path $InstallerPath
